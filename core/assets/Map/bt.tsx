a21d593d890858d76a7aa35c64c27540215bec65
Cardoen Guillaume <190532@student.umons.ac.be>
2019-10-26 13:10:28 +0200

    Deleting BadLogic image + Adding Test Map and Font

---
+++ core/assets/Map/bt.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="batiment" tilewidth="132" tileheight="127" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="132" height="127" source="buildingTiles_002.png"/>
+ </tile>
+ <tile id="1">
+  <image width="99" height="54" source="buildingTiles_013.png"/>
+ </tile>
+ <tile id="2">
+  <image width="99" height="85" source="buildingTiles_016.png"/>
+ </tile>
+ <tile id="3">
+  <image width="99" height="85" source="buildingTiles_031.png"/>
+ </tile>
+ <tile id="4">
+  <image width="99" height="85" source="buildingTiles_032.png"/>
+ </tile>
+</tileset>
--- core/src/be/ac/umons/sgl/mom/MasterOfMonsGame.java
+++ core/src/be/ac/umons/sgl/mom/MasterOfMonsGame.java
@@ -13,7 +13,6 @@ public class MasterOfMonsGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,7 +20,7 @@ public class MasterOfMonsGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+//		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
