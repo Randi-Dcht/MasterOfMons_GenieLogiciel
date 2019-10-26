9c8ec776440c4be0defe918138691beb9ff73460
Cardoen Guillaume <190532@student.umons.ac.be>
2019-10-26 13:02:37 +0200

    Initial commit

---
+++ desktop/src/be/ac/umons/sgl/mom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package be.ac.umons.sgl.mom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import be.ac.umons.sgl.mom.MasterOfMonsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MasterOfMonsGame(), config);
+	}
+}
