4aa61bee2ade78183aa80e6c89bc8cc88f608bc9
190134 <190134@umons.ac.be>
2019-11-14 18:23:01 +0100

    [codeSource] add the objet for the player take (battery)

---
+++ core/src/be/ac/umons/sgl/mom/Objects/Battery.java
@@ -0,0 +1,19 @@
+package be.ac.umons.sgl.mom;
+
+public class Battery extends Objet
+{
+  public Battery(double x,double y)
+  {
+    super(x,y,"Batterie");
+  }
+
+  public void used(People pp)
+  {
+
+  }
+
+  public void make()
+  {
+
+  }
+}
