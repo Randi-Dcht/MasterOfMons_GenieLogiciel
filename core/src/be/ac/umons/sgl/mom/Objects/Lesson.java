8abab846711c72354a764a83467e2eae67eb8fdf
190134 <190134@umons.ac.be>
2019-11-14 18:35:58 +0100

    codeSource -> master of Lesson

--- core/src/be/ac/umons/sgl/mom/Objects/Lesson.java
+++ core/src/be/ac/umons/sgl/mom/Objects/Lesson.java
@@ -1,36 +1,40 @@
 package be.ac.umons.sgl.mom;
 /**
+*Cette classe permet de définir les cours pour tout son cursus scolaire
 *@author Randy Dauchot (étudiant en Sciences informatique)
 */
 public enum Lesson
 {
-	mathInfo1('O',"math pour informatique 1",Lieu.Nimy,1),
-	mathInfo2('O',"math pour informatique 2",Lieu.Nimy,1),
-	algo1Python('O',"algorithme python",Lieu.Nimy,1),
-	algo2Java('F',"algorithme java",Lieu.Nimy,1),
-	fonctionOrdinateur('F',"fonctionnement des ordinateurs",Lieu.Nimy,1),
-	nombreComplexe('F',"nombre complexe",Lieu.Nimy,2),
-	optimisation('O',"optimisation lineaire",Lieu.Nimy,2),
-	anglais('O',"english for sciences",Lieu.Nimy,2),
-	systemeExploitation('O',"systeme exploitation",Lieu.Nimy,2),
-	projetBa1('F',"projet bachelier 1",Lieu.Nimy,1),
-	projetBa2('F',"projet bachelier 2",Lieu.Nimy,2),
-	calculus2('O',"algèbre partie B",Lieu.Nimy,2),
-	economiePolitique('F',"economie politique",Lieu.Nimy,2),
-	realTime('F',"real time computer",Lieu.Nimy,3),
-	reseau('O',"reseau",Lieu.Nimy,2),
-	calculProbabilite('O',"calcul de probabilite",Lieu.Nimy,3),
-	intelligenceArtificielle('O',"intelligence artificielle",Lieu.Nimy,3),
-	compilation('F',"compilation",Lieu.Nimy,3),
-	grapheOptimisation('O',"graphe et optimisation",Lieu.Nimy,3),
-	baseDonnes('F',"base de données",Lieu.Nimy,3),
-	statistique('F',"statistique",Lieu.Nimy,3);
+	MI1('O',"math pour informatique 1",Place.Nimy,1),
+	MI2('O',"math pour informatique 2",Place.Nimy,1),
+	algo1('O',"algorithme python",Place.Nimy,1),
+	algo2('F',"algorithme java",Place.Nimy,1),
+	ftOrdi('F',"fonctionnement des ordinateurs",Place.Nimy,1),
+	nbComplexe('F',"nombre complexe",Place.Nimy,2),
+	opti('O',"optimisation lineaire",Place.Nimy,2),
+	anglais('O',"english for sciences",Place.Nimy,2),
+	OS('O',"systeme exploitation",Place.Nimy,2),
+	projet1('F',"projet bachelier 1",Place.Nimy,1),
+	projet2('F',"projet bachelier 2",Place.Nimy,2),
+	calculus2('O',"algèbre partie B",Place.Nimy,2),
+	ecopol('F',"economie politique",Place.Nimy,2),
+	realTime('F',"real time computer",Place.Nimy,3),
+	reseau('O',"reseau",Place.Nimy,2),
+	calculProba('O',"calcul de probabilite",Place.Nimy,3),
+	intelligen('O',"intelligence artificielle",Place.Nimy,3),
+	compilation('F',"compilation",Place.Nimy,3),
+	grapheOpti('O',"graphe et optimisation",Place.Nimy,3),
+	baseDonnes('F',"base de données",Place.Nimy,3),
+	statistique('F',"statistique",Place.Nimy,3);
 
 	final char type;
 	final String name;
-	final Lieu place;
-	final int annee;
+	final Place place;
+	final int year;
 
+/**
+*Constrcuteur interne
+*/
 	private Lesson(char type,String name, Place place,int year)
 	{
 		this.type  = type;
@@ -39,6 +43,10 @@ public enum Lesson
 		this.year = year;
 	}
 
+/**
+*Cette méthode permey de savoir si le personnage doit aller au cours pour réussir
+*@return true si c'est obliagtoire sinon false
+*/
 	public boolean obligatoryCourse()
 	{
 		if(type == 'O')
@@ -46,12 +54,21 @@ public enum Lesson
 		return false;
 	}
 
+/**
+*Cette méthode permet de dire la localisation du cours sur le campus
+*@return place où se déroule le cours
+*/
 	public Place location()
 	{
 		return place;
 	}
 
-  public boolean take(int year)
+/**
+*Cette méthode permet de savoir si on peut prendre le cours
+*@param year qui est l'année du personnage
+*@return true s'il peut prendre le cours et false sinon
+*/
+	public boolean take(int year)
 	{
 		if(this.year <= year)
 		  return true;
