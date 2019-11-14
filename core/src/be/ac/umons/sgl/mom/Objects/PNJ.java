8d127057bea4a548d9d5b05ceabc65a196e12a04
190134 <190134@umons.ac.be>
2019-11-14 18:07:09 +0100

    [codeSource] add the first method of PNJ

--- core/src/be/ac/umons/sgl/mom/Objects/PNJ.java
+++ core/src/be/ac/umons/sgl/mom/Objects/PNJ.java
@@ -1,2 +1,34 @@
+/**
+*Cette classe permet de définir un personnage qui est piloté par l'ordianteur
+*@author Randy Dauchot (étudiant en Sciences informatique)
+*/
 public class PNJ {
+public class PNJ
+{
+  final String name;
+  private double x;
+  private double y;
+
+  private int agility;
+  private int strength;
+  private int defence;
+
+  public PNJ(String name)
+  {
+    this.name = name;
+  }
+
+  public void move(double x, double y)
+  {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void caracteristique(int strength, int defence, int agility)
+  {
+    this.agility  = agility;
+    this.defence  = defence;
+    this.strength = strength;
+  }
+
 }
