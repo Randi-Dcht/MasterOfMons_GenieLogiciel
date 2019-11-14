4ea38fcda9278372aee78154f8218e2ea5ccd813
190134 <190134@umons.ac.be>
2019-11-14 18:37:15 +0100

    codeSource -> master of Place

--- core/src/be/ac/umons/sgl/mom/Objects/Place.java
+++ core/src/be/ac/umons/sgl/mom/Objects/Place.java
@@ -1,11 +1,17 @@
 package be.ac.umons.sgl.mom;
+
 /**
+*Ensemble des lieux stratégique sur la maps
 *@author Randy Dauchot (étudiant en Sciences informatique)
 */
+
 enum Place
 {
   Nimy,
   Waroque,
+  GrandAmphi,
+  DeVinci,
   PolyHoudaing,
-  Bibliotheque;
+  Bibliotheque,
+  Kot;
 }
