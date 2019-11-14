0c9d96aa5219ef4ace7f6977fac687500da24848
190134 <190134@umons.ac.be>
2019-11-14 18:37:42 +0100

    codeSource -> master of quest1

--- core/src/be/ac/umons/sgl/mom/Objects/Quest1.java
+++ core/src/be/ac/umons/sgl/mom/Objects/Quest1.java
@@ -1,9 +1,60 @@
 package be.ac.umons.sgl.mom;
+import java.util.*;
+/**
+*Cette classe définit ce qu'est une quête de bachelier 1
+*@author Randy Dauchot (étudiant en Sciences informatique)
+*/
+
 public class Quest1 extends Quest
 {
-  public Quest1()
+  public final Lesson[] lesson ={Lesson.MI1,Lesson.MI2,Lesson.algo1,Lesson.algo2,Lesson.ftOrdi,Lesson.projet1};
+  public final GoalsQuest[] goalsQuest = {new FollowLesson(),new GoToTest(),new MeetManyPeople(),new BattleForPlace()};
+  public Quest1(People people)
   {
-    Lesson[] lesson = {null,null,null};
-    super(null,1,lesson);
+    super(null,1,null,people);
   }
+
+  /**
+  *Cette méthode permet de gérer les rencontres entres Persoonage1 et le personnage2
+  *@param other qui est l'autre personnages dans le jeu (aider - combat)
+  */
+    public void meetOther(PNJ other)
+    {
+      eventMaps();
+    }
+
+  /**
+  *Cette méthode renvoie l'object de la quête
+  */
+    public String question()
+    {
+      return "Ta quête si tu l'accepte sera de te faire quelques amis et de participer à un mininum de X cours. Tu devras aussi partciper à tous tes examnens";
+    }
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    public class FollowLesson implements GoalsQuest
+    {
+      public void evenActivity()
+      {
+      }
+    };
+    public class GoToTest implements GoalsQuest
+    {
+      public void evenActivity()
+      {
+      }
+    };
+    public class MeetManyPeople implements GoalsQuest
+    {
+      public int safeFriend = 0;
+      public void evenActivity()
+      {
+      }
+    };
+    public class BattleForPlace implements GoalsQuest
+    {
+      public void evenActivity()
+      {
+      }
+    };
 }
