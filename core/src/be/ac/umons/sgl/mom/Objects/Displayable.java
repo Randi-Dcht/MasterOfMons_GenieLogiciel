d97aa8110a50b8f69599fe760b6b99234a3b9440
190134 <190134@umons.ac.be>
2019-12-02 17:15:26 +0100

    add the super interface for getImage

---
+++ core/src/be/ac/umons/sgl/mom/Objects/Displayable.java
@@ -0,0 +1,8 @@
+package be.ac.umons.sgl.mom.Objects;
+
+public interface Displayable {
+    /**
+    *Cette méthode permet de retourner une image en foinction de l'objet sélectioner
+    */
+    public String getImagePath();
+}
