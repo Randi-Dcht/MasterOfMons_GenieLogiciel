373d807252618a49db9838587eb1786f1c1947ea
190134 <190134@umons.ac.be>
2020-02-01 15:05:07 +0100

    refresh the map

---
+++ core/assets/tsx/Vegetation.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Vegetation" tilewidth="512" tileheight="512" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="202" height="267" source="../Sprite/tree_SE.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="512" source="../Sprite/detail_crystalLarge_N.png"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="512" source="../Sprite/detail_dirtLarge_S.png"/>
+ </tile>
+ <tile id="4">
+  <image width="90" height="78" source="../Sprite/detail_rocksLarge_E.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="512" source="../Sprite/detail_rocksLarge_W.png"/>
+ </tile>
+ <tile id="6">
+  <image width="99" height="110" source="../Sprite/detail_treeLarge_E.png"/>
+ </tile>
+ <tile id="7">
+  <image width="512" height="512" source="../Sprite/detail_tree_N.png"/>
+ </tile>
+</tileset>
