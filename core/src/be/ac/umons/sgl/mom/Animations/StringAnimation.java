679342c51bfecc0f5b2e20a1df69539ee9d6c0e3
Cardoen Guillaume <190532@student.umons.ac.be>
2020-02-04 14:05:45 +0100

    Added StringAnimation + Updated Animation

---
+++ core/src/be/ac/umons/sgl/mom/Animations/StringAnimation.java
@@ -0,0 +1,37 @@
+package be.ac.umons.sgl.mom.Animations;
+
+public class StringAnimation extends Animation<String> {
+
+    double toAddBySecond;
+    double actualNbr = 0;
+
+    public StringAnimation(String to, double time) {
+        super("", to, time);
+        toAddBySecond = (to.length()) / (time / 1000);
+    }
+
+    public StringAnimation(String to, double time, Runnable runningAction, Runnable endCallback) {
+        super("", to, time, runningAction, endCallback);
+    }
+
+    @Override
+    public void update(double dt) {
+        if ((toAddBySecond > 0 && actual.length() >= to.length()) || (toAddBySecond < 0 && actual.length() <= to.length()))
+            return;
+
+        actualNbr += dt * toAddBySecond;
+        actual = to.substring(0, (int)actualNbr);
+        if (runningAction != null)
+            runningAction.run();
+        if ((toAddBySecond > 0 && actual.length() >= to.length()) || (toAddBySecond < 0 && actual.length() <= to.length())) {
+            actual = to;
+            if (endingAction != null)
+                endingAction.run();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
