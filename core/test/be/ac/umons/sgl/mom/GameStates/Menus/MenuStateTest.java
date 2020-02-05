f40341242ebdc5e05eef0a389c2018a2d3868461
Cardoen Guillaume <190532@student.umons.ac.be>
2020-02-05 10:29:24 +0100

    Resolved bug in MenuStateTest

--- core/test/be/ac/umons/sgl/mom/GameStates/Menus/MenuStateTest.java
+++ core/test/be/ac/umons/sgl/mom/GameStates/Menus/MenuStateTest.java
@@ -1,18 +1,22 @@
 package be.ac.umons.sgl.mom.GameStates.Menus;
 
 import be.ac.umons.sgl.mom.Enums.KeyStatus;
+import be.ac.umons.sgl.mom.GraphicalObjects.Controls.Button;
 import be.ac.umons.sgl.mom.GraphicalObjects.Controls.ExtensionsSelector;
 import be.ac.umons.sgl.mom.Managers.GameInputManager;
 import be.ac.umons.sgl.mom.Managers.GameStateManager;
 import be.ac.umons.sgl.mom.Objects.GraphicalSettings;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Classe réprésente la classe MainMenuState. Cette classe de test a principalement pour but de tester la méthode handleInput, en évitant d'allouer trop de ressources.
@@ -25,14 +29,18 @@ public class MenuStateTest extends MainMenuState {
         gsm = Mockito.mock(GameStateManager.class);
         gs = Mockito.mock(GraphicalSettings.class);
         extSel = Mockito.mock(ExtensionsSelector.class);
+        buttons = new ArrayList<>();
+        buttons.add(Mockito.mock(Button.class));
+        buttons.add(Mockito.mock(Button.class));
     }
 
     @BeforeEach
     public void init() {
         MockitoAnnotations.initMocks(this);
-        menuItems = new MenuItem[] { new MenuItem("Master Of Mons", MenuItemType.Title, false),
-                new MenuItem("Play", MenuItemType.Title, true),
+        menuItems = new MenuItem[] { new MenuItem("Master Of Mons", MenuItemType.Title, true),
+                new MenuItem("Play", MenuItemType.Title, false),
                 new MenuItem("Settings", MenuItemType.Title, true)};
+        //setMenuItems(menuItems);
         Mockito.when(gim.getRecentClicks()).thenReturn(new ArrayList<>());
         while (! menuItems[selectedItem].selectable) // Simulate the one in draw.
             selectedItem = (selectedItem + 1) % menuItems.length;
@@ -42,21 +50,21 @@ public class MenuStateTest extends MainMenuState {
     public void testDown() {
         Mockito.when(gim.isKey(Input.Keys.DOWN, KeyStatus.Pressed)).thenReturn(true);
         Mockito.when(gim.isKey(Input.Keys.UP, KeyStatus.Pressed)).thenReturn(false);
-        Assertions.assertEquals(1, selectedItem);
-        handleInput();
-        Assertions.assertEquals(2, selectedItem);
+        Assertions.assertEquals(0, selectedItem);
         handleInput();
         Assertions.assertEquals(1, selectedItem);
+        handleInput();
+        Assertions.assertEquals(0, selectedItem);
     }
 
     @Test
     public void testUp() {
         Mockito.when(gim.isKey(Input.Keys.DOWN, KeyStatus.Pressed)).thenReturn(false);
         Mockito.when(gim.isKey(Input.Keys.UP, KeyStatus.Pressed)).thenReturn(true);
-        Assertions.assertEquals(1, selectedItem);
-        handleInput();
-        Assertions.assertEquals(2, selectedItem);
+        Assertions.assertEquals(0, selectedItem);
         handleInput();
         Assertions.assertEquals(1, selectedItem);
+        handleInput();
+        Assertions.assertEquals(0, selectedItem);
     }
 }
