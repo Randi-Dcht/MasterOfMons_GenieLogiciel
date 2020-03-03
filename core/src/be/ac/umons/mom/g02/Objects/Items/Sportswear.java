ebcd4e1c8326cf0a2a9b747daa8b751cade14b67
Maxime1299 <57593135+Maxime1299@users.noreply.github.com>
2020-03-03 12:03:34 +0100

    Add update in sportswear

--- core/src/be/ac/umons/mom/g02/Objects/Items/Sportswear.java
+++ core/src/be/ac/umons/mom/g02/Objects/Items/Sportswear.java
@@ -1,8 +1,12 @@
 package be.ac.umons.mom.g02.Objects.Items;
 
+import be.ac.umons.mom.g02.Events.Notifications.UseItem;
+import be.ac.umons.mom.g02.Events.SuperviserNormally;
 import be.ac.umons.mom.g02.Objects.Characters.People;
 
 public class Sportswear extends Items {
+
+    private double reVisible = 0;
 
     public Sportswear() {
         super("Sportswear");
@@ -10,28 +14,21 @@ public class Sportswear extends Items {
 
     @Override
     public void used(People pp){
-        //pp.setSpeed();
-    }
-
-    public void make(double time) {
-
+        pp.setSpeed(2); //on double sa vitesse quand il possede cet item
+        visibly();
+        SuperviserNormally.getSupervisor().getEvent().notify(new UseItem(this));
     }
 
     @Override
     public double getObsolete() {
         return 0;
     }
-
-    /**
-     * It help to know what do the item
-     * @return the utility of the item
-     */
-    public String question() {
-        return "sportswear";
-    }
+    // Il ne me faut pas cette methode car ça reste tout le temps vsible et ça ne casse pas
 
     @Override
     public void update(double dt) {
-
+        //Je n'ai pas besoin de cette methode car mon objet ne s'use pas avec le temps
     }
+
+
 }
