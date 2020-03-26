535e3c21a4d34e6bd00de93e93bc97f2eef54dec
Guillaume Cardoen <190532@student.umons.ac.be>
2020-03-26 14:08:26 +0100

    Took into account the transparency while drawing ColorSelector

--- core/src/be/ac/umons/mom/g02/GraphicalObjects/Controls/ColorSelector.java
+++ core/src/be/ac/umons/mom/g02/GraphicalObjects/Controls/ColorSelector.java
@@ -3,7 +3,9 @@ package be.ac.umons.mom.g02.GraphicalObjects.Controls;
 import be.ac.umons.mom.g02.Helpers.StringHelper;
 import be.ac.umons.mom.g02.Managers.GameInputManager;
 import be.ac.umons.mom.g02.Objects.GraphicalSettings;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import java.awt.*;
@@ -46,12 +48,15 @@ public class ColorSelector extends Control {
 
     @Override
     public void draw(Batch batch, Point pos, Point size) {
+        Gdx.gl.glEnable(GL30.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL30.GL_SRC_ALPHA, GL30.GL_ONE_MINUS_SRC_ALPHA);
         super.draw(batch, pos, size);
         sr.begin(ShapeRenderer.ShapeType.Filled);
         sr.setColor(selectedColor);
         sr.ellipse(pos.x + leftMargin, pos.y + topMargin, size.y - 2 * topMargin, size.y - 2 * topMargin);
         sr.end();
         tb.draw(batch, new Point(pos.x + size.y - 2 * topMargin + 2 * leftMargin, pos.y), new Point(size.x - size.y + 2 * topMargin - 2 * leftMargin, size.y));
+        Gdx.gl.glDisable(GL30.GL_BLEND);
     }
 
     @Override
