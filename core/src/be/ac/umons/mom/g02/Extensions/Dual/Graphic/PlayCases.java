fcfcabb4f1c945e41a714bc453bd277f4dc5eab7
Randi_Tour_Linux <190134@umons.ac.be>
2020-03-27 12:05:17 +0100

    add the first class to call after choose the dual quest

--- core/src/be/ac/umons/mom/g02/Extensions/Dual/Graphic/PlayCases.java
+++ core/src/be/ac/umons/mom/g02/Extensions/Dual/Graphic/PlayCases.java
@@ -1,4 +1,17 @@
 package be.ac.umons.mom.g02.Extensions.Dual.Graphic;
 
-public class PlayCases {
+import be.ac.umons.mom.g02.Objects.GraphicalSettings;
+
+/**
+ * This class allows to add in the PlayingState the number of cases on the maps for the extension Dual (MoreCaseMons)
+ */
+public class PlayCases extends PlayingStateDual
+{
+    /**
+     * @param gs The game's graphical settings
+     */
+    public PlayCases(GraphicalSettings gs)
+    {
+        super(gs);
+    }
 }
--- core/src/be/ac/umons/mom/g02/Extensions/Dual/Logic/Enum/TypeDual.java
+++ core/src/be/ac/umons/mom/g02/Extensions/Dual/Logic/Enum/TypeDual.java
@@ -2,21 +2,23 @@ package be.ac.umons.mom.g02.Extensions.Dual.Logic.Enum;
 
 
 import be.ac.umons.mom.g02.Enums.Maps;
+import be.ac.umons.mom.g02.Extensions.Dual.Graphic.PlayingStateDual;
 import be.ac.umons.mom.g02.Extensions.Dual.Logic.Quest.BattlePeople;
 import be.ac.umons.mom.g02.Extensions.Dual.Logic.Quest.DualUnderQuest;
 import be.ac.umons.mom.g02.Extensions.Dual.Logic.Quest.MoreCasesMons;
 import be.ac.umons.mom.g02.Extensions.Dual.Logic.Quest.SurvivorVsMobile;
 import be.ac.umons.mom.g02.Extensions.Dual.Logic.Quest.TakeFlag;
+import be.ac.umons.mom.g02.GameStates.PlayingState;
 import java.awt.Point;
 
 
 /***/
 public enum TypeDual
 {
-    DualPlayer(     "", BattlePeople.class    ,Maps.DualKiosk,new Point(15,15),new Point(15,15)),
-    CatchFlag(      "", TakeFlag.class        ,Maps.DualPark ,new Point(7,24),new Point(8,24)),
-    Survivor(       "", SurvivorVsMobile.class,Maps.DualKiosk,new Point(15,15),new Point(15,15)),
-    OccupationFloor("", MoreCasesMons.class   ,Maps.DualPark ,new Point(8,24),new Point(8,24));
+    DualPlayer(     "", BattlePeople.class    , PlayingStateDual.class,Maps.DualKiosk,new Point(15,15),new Point(15,15)),
+    CatchFlag(      "", TakeFlag.class        , PlayingStateDual.class,Maps.DualPark ,new Point(7,24),new Point(8,24)),
+    Survivor(       "", SurvivorVsMobile.class, PlayingStateDual.class,Maps.DualKiosk,new Point(15,15),new Point(15,15)),
+    OccupationFloor("", MoreCasesMons.class   , PlayingStateDual.class,Maps.DualPark ,new Point(8,24),new Point(8,24));
 
 
     /***/
@@ -25,6 +27,9 @@ public enum TypeDual
 
     /***/
     final Class<? extends DualUnderQuest> start;
+
+
+    final Class<? extends PlayingState> graphic;
 
 
     /***/
@@ -40,13 +45,14 @@ public enum TypeDual
 
 
     /***/
-    TypeDual(String nameQuest, Class<? extends DualUnderQuest> startClass, Maps map, Point playerOnePos, Point playerTwoPos)
+    TypeDual(String nameQuest, Class<? extends DualUnderQuest> startClass,Class<? extends PlayingState> graphic,Maps map, Point playerOnePos, Point playerTwoPos)
     {
         name  = nameQuest;
         start = startClass;
         maps  = map;
         pointOne = playerOnePos;
         pointTwo = playerTwoPos;
+        this.graphic = graphic;
     }
 
 
@@ -63,6 +69,11 @@ public enum TypeDual
         return pointTwo;
     }
 
+
+    public Class<? extends PlayingState> getGraphic()
+    {
+        return graphic;
+    }
 
     /***/
     public String getName()
