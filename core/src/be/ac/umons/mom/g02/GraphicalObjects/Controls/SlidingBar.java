a72469f382c5a1c7f4ac224b33d0203f09524582
Guillaume Cardoen <190532@student.umons.ac.be>
2020-03-29 11:45:16 +0200

    Fixed a few bugs in SlidingBar + added a SlidingBar at some places

--- core/src/be/ac/umons/mom/g02/GraphicalObjects/Controls/SlidingBar.java
+++ core/src/be/ac/umons/mom/g02/GraphicalObjects/Controls/SlidingBar.java
@@ -28,9 +28,17 @@ public class SlidingBar extends Control {
      */
     protected boolean isMoving = false;
     /**
+     * The minimum value defined by the user.
+     */
+    protected int minValue;
+    /**
      * The maximum value defined by the user.
      */
     protected int maxValue;
+    /**
+     * The maximum value defined by the user on which this bar can go.
+     */
+    protected int maxSlidingValue;
     /**
      * The actual value of this bar
      */
@@ -47,7 +55,7 @@ public class SlidingBar extends Control {
         super(gs);
         sr = new ShapeRenderer();
         sr.setAutoShapeType(true);
-        actualColor = Color.WHITE;
+        actualColor = gcm.getColorFor("foreground");
     }
 
     /**
@@ -58,12 +66,12 @@ public class SlidingBar extends Control {
     @Override
     public void draw(Batch batch, Point pos, Point size) {
         if (posX < 0)
-            posX = pos.x + (float)size.x / 2;
+            posX = pos.x + (float)size.y / 2;
         super.draw(batch, pos, size);
         sr.setColor(actualColor);
         sr.begin(ShapeRenderer.ShapeType.Filled);
         float posY = pos.y + (float)size.y / 2;
-        sr.rectLine(pos.x + size.y, posY, pos.x + size.x - size.y / 2, posY, (float)size.y / 20);
+        sr.rectLine(pos.x + size.y / 2, posY, pos.x + size.x - size.y / 2, posY, (float)size.y / 20);
         sr.circle(posX, posY, size.y / 2);
         sr.end();
     }
@@ -74,20 +82,28 @@ public class SlidingBar extends Control {
     @Override
     public void handleInput() {
         if (gim.isMouseJustClicked() &&
-            new Rectangle((int)(posX - height / 2), MasterOfMonsGame.HEIGHT - (y + height / 2), (int)(posX + height / 2), MasterOfMonsGame.HEIGHT - (y - height / 2)).contains(gim.getLastMousePosition())) {
+            new Rectangle((int)(posX - height / 2), MasterOfMonsGame.HEIGHT - (y + height), height, height).contains(gim.getLastMousePosition())) {
             isMoving = true;
         } else if (gim.isMouseJustUnclicked())
             isMoving = false;
         if (isMoving) {
+            float oldPosX = posX;
             posX = gim.getLastMousePosition().x;
-            if (posX < x + height / 2)
-                posX = x + height / 2;
-            else if (posX > x + width - height / 2)
-                posX = x + width - height / 2;
-            int tmpValue = Math.round((posX - x - height / 2) / (width - height) * maxValue);
-            if (tmpValue != value && onValueChanged != null)
+            if (posX < x + (float)height / 2)
+                posX = x + (float)height / 2;
+            else if (posX > x + width - (float)height / 2)
+                posX = x + width - (float)height / 2;
+            double tmpValue = minValue + (posX - x - (float)height / 2) / (width - (float)height) * (maxValue - minValue);
+
+            if (tmpValue > maxSlidingValue) {
+                posX = oldPosX;
+                return;
+            }
+
+            int tmp = minValue + value;
+            value = (int)tmpValue + (tmpValue - (int)tmpValue > 0.5 ? 1 : 0); // Math.round -> +inf (from doc)
+            if (tmp != value && onValueChanged != null)
                 onValueChanged.run();
-            value = tmpValue;
         }
     }
 
@@ -100,10 +116,33 @@ public class SlidingBar extends Control {
     }
 
     /**
+     * @param minValue The minimum value this bar can return
+     */
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+        if (value < minValue)
+            value = minValue;
+    }
+
+    /**
      * @param maxValue The maximum value this bar can return
      */
     public void setMaxValue(int maxValue) {
         this.maxValue = maxValue;
+        this.maxSlidingValue = maxValue;
+        if (value > maxValue)
+            value = maxValue;
+//        setActualValue(Math.round((posX - x - height / 2) / (width - height) * maxValue));
+    }
+
+    /**
+     * @param maxValue The maximum value this bar can go to
+     */
+    public void setMaxSlidingValue(int maxValue) {
+        this.maxSlidingValue = maxValue;
+        if (value > maxValue)
+            value = maxValue;
+//        setActualValue(Math.round((posX - x - height / 2) / (width - height) * maxValue));
     }
 
     /**
@@ -118,7 +157,11 @@ public class SlidingBar extends Control {
      * @param value The actual value of this bar
      */
     public void setActualValue(int value) {
-        posX = x + ((float)value / maxValue) * width;
+        if (value < minValue)
+            value = minValue;
+        else if (value > maxValue)
+            value = maxValue;
+        posX = x + height / 2 + ((float)value / maxValue) * (width - height);
         this.value = value;
     }
 
